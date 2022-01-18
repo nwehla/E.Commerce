20869ec5fef3082da06a6547072524770cb8f4d0
nwehla <nwehlabatoum@laposte.net>
2022-01-19 00:59:52 +0100

    mise en place du caroussel et du bootstrap

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function index(): Response
+    {
+        return $this->render('register/inscription.html.twig');
+    }
+}
